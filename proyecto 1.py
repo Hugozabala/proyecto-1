9ada67489f8482e6717e43c26c31466d8102ccd1
German Escobar <germanjosearielescobarescobar@gmail.com>
2025-08-17 20:24:21 -0600

    menu3

--- proyecto 1.py
+++ proyecto 1.py
@@ -50,9 +50,21 @@ class Producto:
 
 
 class Ordenar:
-    pass
+    def quicksort(self, lista, criterio):
+        if len(lista) <= 1:
+            return lista
+        pivote = lista[0]
+
+        menores = [x for x in lista[1:] if x < pivote]
+        iguales = [x for x in lista if x == pivote]
+        mayores = [x for x in lista[1:] if x > pivote]
+
+        return quick_sort(menores) + iguales + quick_sort(mayores)
+
 class Buscar:
-    pass
+    def Buscar(self):
+        pass
+
 class Inventario:
     def Agregar(self):
             try:
